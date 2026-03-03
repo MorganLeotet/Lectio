034482b3eda31c799a8d3fd8d23840b1a8b51fc1
Morgan Léotet <mrleotetm@gmail.com>
2026-03-03 17:33:07 +0100

    finalize login system and dynamic header authentication

--- public/components/js/headerAuth.js
+++ public/components/js/headerAuth.js
@@ -1,41 +1,35 @@
-import { auth } from "../core/auth.js";
+import { auth } from "/js/core/auth.js";
 
 export function initHeaderAuth() {
 
     const loginBtn = document.querySelector(".login-btn");
-    const userInfo = document.querySelector(".user-info");
     const welcomeText = document.querySelector(".welcome-text");
-    const logoutBtn = document.querySelector(".logout-btn");
 
-    if (!loginBtn || !userInfo) return;
+    if (!loginBtn) return;
 
     function updateHeader() {
-
         const user = auth.getUser();
 
         if (user) {
-        loginBtn.classList.add("hidden");
-        userInfo.classList.remove("hidden");
-
-        if (welcomeText) {
-            welcomeText.textContent = `Bienvenue, ${user.name}`;
-        }
+        loginBtn.textContent = "Déconnexion";
+        welcomeText.textContent = `Bienvenue ${user.name}`;
         } else {
-        loginBtn.classList.remove("hidden");
-        userInfo.classList.add("hidden");
+        loginBtn.textContent = "Connexion";
+        welcomeText.textContent = "";
         }
     }
 
-    // première mise à jour
-    updateHeader();
+    loginBtn.addEventListener("click", () => {
+        const user = auth.getUser();
 
-    // écoute changement login/logout
-    window.addEventListener("authChange", updateHeader);
-
-    // bouton logout
-    if (logoutBtn) {
-        logoutBtn.addEventListener("click", () => {
+        if (user) {
         auth.logout();
-        });
-    }
+        updateHeader();
+        window.location.href = "/index.html";
+        } else {
+        document.dispatchEvent(new Event("openLogin"));
+        }
+    });
+
+    updateHeader();
 }
--- public/js/lectio.js
+++ public/js/lectio.js
@@ -2,13 +2,13 @@
 
 console.log("Lectio JS chargé");
 
-import { authGuard } from "./core/authGuard.js";
+import { authGuard } from "../js/core/authGuard.js";
 import { initAccessibility } from "./pages/accessibility.js";
 import { loadLogin } from "./pages/loadLogin.js";
 import { initHome } from "./pages/home.js";
 import { initLibrary } from "./pages/library.js";
 import { initBook } from "./pages/book_connected.js";
-import { initHeaderAuth } from "./components/headerAuth.js";
+import { initHeaderAuth } from "../components/js/headerAuth.js";
 
 /* GUARD */
 authGuard();
